fb0833785d9c9a62ca72784df103f55b05350fde
umeshregmi <regmiumesh51@gmail.com>
2025-06-19 10:01:15 +0545

    day1.py

---
+++ day1.py
@@ -0,0 +1,131 @@
+# i = 5
+# print(i)
+# print(i)
+# print(i)
+
+# this is a comment
+
+# i = 5
+# i = 10
+# print(i)
+
+# my_age = 20
+
+
+# PI = 3.14
+# EARTH_GRAVITY = 9.8
+
+
+# int float
+
+salary = 100.24
+i = 20
+
+# type() # data type of a value/ variable
+
+# print(type(i))
+# print(type(salary))
+
+
+# x, y, z = "Orange", "Banana", "Cherry"
+
+
+# arithmetic operators
+
+# print(1+1)
+# print(1 - 1)
+# print(5 * 3)
+# print(50 / 10)
+
+# print(1.1+1)
+# print(1.5 - 1)
+# print(5 * 3)
+# print(50 / 10)
+
+
+# modulo operator % 
+# print(1 % 2)
+# print(3 % 2)
+# print(5 % 2)
+
+# print(2 % 2)
+# print(4 % 2)
+# print(6 % 2)
+
+# print(8 % 5)
+
+# print(2 ** 3)  # 2 ^ 3
+
+# floor division
+# print(5 // 2)
+
+# boolean 
+# is_summer = True
+# is_winter = False
+
+# print(type(is_summer))
+# print(type(is_winter))
+
+# print(True + True + True)
+# print(True / True)
+
+# print(not True)
+# print(not False)
+
+# print(True and True and True and True)
+# print(True and True and True and False)
+
+# print(True or True or True or False)
+
+
+# precedence of logical operator
+# not and or 
+# print(True and not True or False and True and not True)
+
+
+# comparison operator 
+# == 
+# != 
+# > 
+# < 
+# >=
+# <=
+
+# print(5 == 5 == 5 == 6)
+# print(5 != 6)
+# print(5 > 6)
+# print(5 < 6)
+
+# print(5 >= 3)
+# print(5 >= 5)
+# print(5 >= 8)
+
+
+# chars or sequenc of characters enclosed within '', "", '''
+
+# name = 'ram'
+# last_name = "hari"
+# address = '''balaju'''
+# print(type(name))
+# print(type(last_name))
+# print(type(address))
+
+# string concatenation
+# joing two or more strings
+
+# print('ram' + 'hari')
+# print(1 + 1)
+# print('1' + '1')
+
+
+
+
+
+
+
+
+
+
+
+
+
