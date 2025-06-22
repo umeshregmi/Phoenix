79e93f66fc64ae502e2f87902f7cdffcca0050a9
umeshregmi <regmiumesh51@gmail.com>
2025-06-22 07:12:11 +0545

    day2

---
+++ day2.py
@@ -0,0 +1,83 @@
+# name = 'umesh'
+# print(len(name))
+# print(len('   '))
+# print(len(''))
+
+# membership operator
+# in, not in
+
+# print('u' in 'umesh')
+# print('U' in 'umesh')
+# print('um' in 'umesh')
+# print('ush' in 'umesh')
+
+# print('ush' not in 'umesh')
+# print(not 'ush' in 'umesh')
+
+# a = "python's class is awesome"
+# a = 'python"s class is awesome'
+
+
+# name = 'phoenix'
+# print(name[1])
+# print(name[-1])
+# print(name[10])
+# print(name[2])
+
+# name = 'phoenix college'
+# name[2:5]
+# name[8:15]
+# name[::2]
+# name[:5:2]
+
+# name = 'phoenix'
+# print(name[::-1])
+
+name = 'phoenix college'
+address = 'MAITIdevi'
+# print(name.capitalize())
+# print(name)
+# print(name.title())
+# print(name.upper())
+# print(address.lower())
+# name.endswith('college')
+
+# type casting/ conversion
+
+# int()
+# float()
+# str()
+# bool()
+
+# print(int(1.1))
+# print(int(True))
+# print(int(False))
+# print(int('1') + int('1'))
+
+# print(float(1))
+# print(float(True))
+# print(float(False))
+# print(float('1.1') + float('1.1'))
+
+# print(str(1) + str(1))
+
+
+# print(bool(0))
+# print(bool(-1))
+# print(bool(1.9))
+
+# print(bool(0.00))
+# print(bool(0.000009))
+
+# print(bool('ram'))
+# print(bool('  '))
+# print(bool('')) 
+
+
+
+
+
+
+
+
+
