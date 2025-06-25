b937ac42e237a3d432022b05706919c358effea0
umeshregmi <regmiumesh51@gmail.com>
2025-06-25 09:52:52 +0545

    day3.py

---
+++ day3.py
@@ -0,0 +1,128 @@
+# non-primitive 
+#     list tuple set dictionary 
+
+# stores collection of values 
+
+# list 
+
+# a = [] # preferred
+# b = list()
+
+# print(type(a))
+# print(type(b))
+
+# my_list = [1,2,3,4,5]
+# names = ['ram', 'shyam', 'hari']
+
+# properties 
+    # heteregenous
+    # indexed/ordered
+    # mutable/changeable
+    # allow duplicates
+
+# l = [1, 1.1, True, 'ram']
+# print(l[0])
+# print(l[-1])
+
+# a = [1,1,1,1,2,2,3,3,4]
+# print(a)
+
+# names = ['ram', 'shyam', 'hari']
+# print(len(names))
+# print('ram' in names)
+
+# names = ['ram', 'shyam', 'hari']
+# names[0] = 'ramesh'
+# print(names)
+
+# del names[0]
+# print(names)
+
+# a = ['ram', 'shyam', 'hari']
+# del a
+# print(a)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.append("orange")
+# thislist.append('mango')
+# print(thislist)
+# print(len(thislist))
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.insert(1, "orange")
+# thislist.insert(2, 'mango')
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.remove('apple')
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.pop()
+# thislist.pop()
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.pop(0)
+# print(thislist)
+
+
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.clear()
+# print(thislist)
+
+
+# list1 = ["a", "b", "c"]
+# list2 = [1, 2, 3]
+
+# list3 = list1 + list2
+# print(list3)
+
+# cars = ['Ford', 'BMW', 'Volvo']
+# cars.sort()
+# print(cars)
+
+# fruits = ['apple', 'banana', 'cherry']
+# fruits.reverse()
+# print(fruits)
+
+fruits = ['apple', 'banana', 'cherry']
+# fruits.sort()
+# fruits.reverse()
+
+# fruits.sort(reverse=True)
+# print(fruits)
+
+
+
+# tuple ---------
+# a = ()  # preferred
+# b = tuple()
+
+
+# days = ('sun', 'mon', 'tues', 'wed', 'thurs')
+# print(days[0])
+# days[0] = 'ram'
+
+# print(len(days))
+# print('sun' in days)
+
+# a = (1,)
+# print(type(a))
+
+
+# a,b,c = (1,2,3)
+# print(a,b,c)
+
+
+# tuple1 = ("a", "b" , "c")
+# tuple2 = (1, 2, 3)
+
+# tuple3 = tuple1 + tuple2
+# print(tuple3)
+
+
+# thisset = {"apple", "banana", "cherry", True, 1, 2}
+
+# print(thisset)
