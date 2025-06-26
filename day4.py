62da58d038dba33187e8b8c532ab8dd4027afdcc
umeshregmi <regmiumesh51@gmail.com>
2025-06-26 09:48:43 +0545

    day4

---
+++ day4.py
@@ -0,0 +1,120 @@
+# stores key value pair
+
+# a = {}  # preferrred way
+# b = dict()
+
+# grades = {'ram': 25, 'shyam': 35, 'hari': 70}
+
+# heterogenous
+# mutable
+
+# keys must be unique
+# grades = {'ram': 25, 'shyam': 35, 'ram': 70}
+# print(grades)
+
+# print('ram' in grades)
+# print(len(grades))
+
+# grades = {'ram': 25, 'shyam': 35, 'hari': 70}
+# print(grades['ram'])  # access
+# print(grades['rama']) # gives error
+# grades['ram'] = 40  # update
+# print(grades)
+
+# grades = {'ram': 25, 'shyam': 35, 'hari': 70}
+# grades['ramesh'] = 50
+# print(grades)
+
+# grades = {'ram': 25, 'shyam': 35, 'hari': 70}
+# print(grades.keys())
+# print(grades.values())
+# print(grades.items())
+
+# grades = {'ram': 25, 'shyam': 35, 'hari': 70}
+# del grades['ram']
+# print(grades)
+
+
+# info = {'name': 'ram', 'address': {'city': 'maitidevi', 'district': 'ktm' }}
+# print(info['name'])
+# print(info['address']['city'])
+# print(info['address']['district'])
+
+# info['address']['district'] = 'pokhara'
+# print(info)
+
+# info.clear()
+# print(info)
+
+
+# --------------------------
+
+# if else / conditional statement
+# if condition:
+#     statement
+
+
+# if 3 > 3:
+#     print('i am if')
+#     print('bye')
+# else:
+#     print('i am else')    
+# print('end')
+
+# if 2 > 3:
+#     print('first if')
+# elif 50 > 8:
+#     print('first elif')
+# elif 4 == 4:
+#     print('second elif')
+# elif 9 > 8:
+#     print('third elif')
+# else:
+#     print('else')
+
+# if 3>8:
+#     pass
+# else:
+#     print('hi')
+
+
+# x = 19
+# if x > 10:
+#     print("Above ten,")
+#     if x > 20:
+#         print("and also above 20!")
+#     else:
+#         print("but not above 20.")
+# else:
+#     print('i am else')
+
+
+# a = 2000
+# b = 330
+# print("A") if a > b else print("B")
+
+# if a > b:
+#     print("A")
+# else:
+#     print('B')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
