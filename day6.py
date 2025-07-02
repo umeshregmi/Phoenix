49628c3fb0f920ac8eccd44fa4780496ad51505a
umeshregmi <regmiumesh51@gmail.com>
2025-07-02 09:36:39 +0545

    day6

---
+++ day6.py
@@ -0,0 +1,107 @@
+# def add(i,j):
+#     print(i+j)
+
+# def add(i,j,k):
+#     print(i+j+k)
+
+# def add(i,j,k,l):
+#     print(i+j+k+l)
+
+# # function overloading
+
+# add(2,3)
+# add(2,3,4)
+# add(2,3,4,5)
+
+
+# multiple/variable arguments
+    # multiple length positional arguments
+        # represented by *args
+        # stores values in tuple
+
+    # multiple length keyword arguments
+        # represnted by **kwargs
+        # stores values in dictionary
+
+# def add(*args):
+#     print(sum(args))
+
+# add()
+# add(1)
+# add(1,2)
+# add(1,2,3)
+# add(1,2,3,4)
+
+# def info(**kwargs):
+#     print(kwargs)
+
+# info()
+# info(fn='ram')
+# info(fn='ram', ln='hari')
+# info(fn='ram', ln='hari', age=25)
+
+
+
+
+
+
+
+# def add(i,j):
+#     total = i+j
+#     print(total)
+
+# add(2,3)
+# print(total)
+
+
+
+
+# return 
+    # it returns the value from the function to the  place where it is being called.
+    # it acts as break statement to the function.
+    
+
+
+# def add(i,j):
+#     total = i+j
+#     return total
+#     print('hi')
+
+# result = add(2,3)
+# print(result)
+
+
+# def add(i,j):
+#     total = i+j
+#     print(total)
+
+# result = add(2,3)
+# print(result)
+
+
+
+# write a function that takes a number as a parameter as returns True if the number if even else returns False
+
+# write a function that takes a string as a parameter and returns the length of the string 
+
+
+# write a function that takes a string as a parameter and returns the last character of the string.
+
+# write a function that takes a string as a parameter and returns the reverse of the string.
+
+# write a function that takes a string as a parameter and returns True if the string is palindrome else returns False ingore case sensitivity.
+
+# write a function that takes a string as a parameter and returns the first vowel of the string.
+
+# write a function that takes a string as a parameter and returns the first consonant of the string.
+
+# write a function that takes a string as a parameter and returns the total number of vowels in the string.
+
+# write a function that takes a string as a parameter and returns the total number of consonants in the string.
+
+
+
+
+
+
+
