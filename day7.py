8c8c609ab672ff929ac8b011cbb22ca750a526f2
umeshregmi <regmiumesh51@gmail.com>
2025-07-03 10:00:05 +0545

    day7

---
+++ day7.py
@@ -0,0 +1,45 @@
+# list comprehension
+
+# it is a way to create a new list from an existing list.
+# very efficient way to create from an existing one.
+
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+# without comprehension
+# even = []
+# for num in numbers:
+#     if num % 2 == 0:
+#         even.append(num)
+# print(even)
+
+# with comprehension
+# even = [num for num in numbers if num % 2 == 0]
+
+# names = ['ram', 'shyam', 'hari', 'rita', 'Ramesh']
+# new_names = [name for name in names if name.lower() == 'r']
+
+# print(list(range(50)))
+
+# new_num = [num for num in range(51) if num % 5 == 0 or num % 10 == 0]
+# print(new_num)
+
+
+# def add(i,j):
+#     return i + j
+
+
+# s = lambda i,j: i + j
+# result = s(2,3)
+# print(result)
+
+
+# package and modules 
+
+
+
+
+
+
+
+
+
